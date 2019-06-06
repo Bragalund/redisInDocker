2c78540ce7bfcde330a15e6f487986dfa01a0ab5
bragalund <henrikbakkelund@gmail.com>
2019-06-06 21:27:23 +0200

    Fixed some if-statements.

--- installAnsibleWithPip.sh
+++ installAnsibleWithPip.sh
@@ -5,7 +5,6 @@ command -v pip >/dev/null 2>&1 || {
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py;
 python get-pip.py --user; 
 sudo ln -s ~/.local/bin/pip /usr/local/bin;
-exit 1;
 }
 
 # install ansible with pip
--- runMe.sh
+++ runMe.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # install ansible
-if [[ command -v ansible ] -eq 0 ]; then
-    echo 'Ansible is already installed...'
+if hash ansible 2>/dev/null; then
+    echo 'Ansible is already installed...';
 elif [[uname -a | grep -i ubuntu] -eq 0]; then
     echo "installing ansible with apt...";
     sh installAnsibleWithApt.sh
 else 
-    echo "installing ansiebl with pip...";
+    echo "installing ansible with pip...";
     sh installAnsibleWithPip.sh;
 fi
 
 # install docker with ansible
-ansible-playbook main.yml --ask-become-pass
+ansible-playbook main.yml --ask-become-pass;
 
 # Set up docker volumes
-docker volume create --name some-docker-volume;
+docker volume create --name redis-docker-volume;
 
 # Pull redis image
 docker pull redis;
 
 # Run redis image and assign volume
-docker run -p 6379:6379 -d redis -v some-docker-volume; 
+docker run -p 6379:6379 -d -v redis-docker-volume --name redis-docker-instance redis; 
 
 
