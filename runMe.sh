26a03088d3db59a97d237c64bf71323a588c1d0c
bragalund <henrikbakkelund@gmail.com>
2019-06-06 20:53:37 +0200

    Added some stuff.

--- runMe.sh
+++ runMe.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # install ansible
-if [ command -v ansible -eq 0 ]; then
+if [[ command -v ansible ] -eq 0 ]; then
     echo 'Ansible is already installed...'
 elif [[uname -a | grep -i ubuntu] -eq 0]; then
+    echo "installing ansible with apt...";
     sh installAnsibleWithApt.sh
 else 
+    echo "installing ansiebl with pip...";
     sh installAnsibleWithPip.sh;
 fi
 
@@ -13,12 +15,12 @@ fi
 ansible-playbook main.yml --ask-become-pass
 
 # Set up docker volumes
-docker volume create --name some-docker-volume
+docker volume create --name some-docker-volume;
 
 # Pull redis image
 docker pull redis;
 
 # Run redis image and assign volume
-docker run -p 6379:6379 redis -v some-docker-volume
+docker run -p 6379:6379 -d redis -v some-docker-volume; 
 
 
